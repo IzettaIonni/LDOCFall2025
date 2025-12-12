eecd7fbf79ea5e8c2ca56f8a1e0034de993f4733
Izetta <izettaionni@mail.ru>
2025-12-12 10:53:45 +0500

    added sys5

---
+++ LDOCsys4/postgres_healthcheck.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+CONTAINER_NAME="mypostgre" 
+
+if podman ps | grep -q "$CONTAINER_NAME" && podman ps | grep "$CONTAINER_NAME" | grep -q "Up"; then
+    echo "✅ Container '$CONTAINER_NAME' is running (Up)"
+    exit 0
+else
+    echo "❌ Container '$CONTAINER_NAME' is not running"
+    exit 1
+fi
